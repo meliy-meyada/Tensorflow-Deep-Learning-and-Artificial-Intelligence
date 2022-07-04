493317574bb57f2475a142023f3fb133ee91efbc
Meyada <79361511+meliy-meyada@users.noreply.github.com>
2022-07-04 15:37:13 +0700

    check lib

---
+++ TF2.0 Demo/tf2.0_demo.py
@@ -0,0 +1,36 @@
+import sklearn
+print("sklearn " + sklearn.__version__)
+
+import numpy
+print("numpy " + numpy.__version__)
+
+import scipy
+print("scipy " + scipy.__version__)
+
+import pandas
+print("pandas " + pandas.__version__)
+
+import seaborn
+print("seaborn " + seaborn.__version__)
+
+import wordcloud
+print("wordcloud " + wordcloud.__version__)
+
+import bs4
+print("bs4 " + bs4.__version__)
+
+import requests
+print("requests " + requests.__version__)
+
+import theano
+print("theano " + theano.__version__)
+
+import networkx
+print("networkx " + networkx.__version__)
+
+import cv2
+print("cv2 " + cv2.__version__)
+
+import gym
+print("gym " + gym.__version__)
+
